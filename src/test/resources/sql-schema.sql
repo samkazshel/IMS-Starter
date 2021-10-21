cd55e170946853c12bb787e6133679ed64821b37
samkazshel <samsheldon779@gmail.com>
2021-10-21 17:16:13 +0100

    updating sql-schema file to match the tables created in mySQL

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,3 +1,4 @@
+CREATE database IMS;
 DROP TABLE IF EXISTS `customers`;
 
 CREATE TABLE IF NOT EXISTS `customers` (
@@ -6,3 +7,17 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `products`(
+	`product_id` INT NOT NULL auto_increment,
+    `product_name` VARCHAR(50) NOT NULL,
+    `product_value` DECIMAL NOT NULL,
+    PRIMARY KEY (`product_id`)
+);
+
+--CREATE TABLE IF NOT EXISTS `orders`(
+	--`order_id` INT NOT NULL auto_increment,
+    --need to complete
+--);
+
+--to do create tables for orders and order-items table
