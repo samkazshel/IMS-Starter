c5936ad1f144906df2dfeb754a3cdcc3d68d0dd2
samkazshel <samsheldon779@gmail.com>
2021-10-25 17:10:53 +0100

    IQ-18 #comment added 4 tables to the mySQL database and into the sql schema file

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -15,9 +15,20 @@ CREATE TABLE IF NOT EXISTS `products`(
     PRIMARY KEY (`product_id`)
 );
 
---CREATE TABLE IF NOT EXISTS `orders`(
-	--`order_id` INT NOT NULL auto_increment,
-    --need to complete
---);
+CREATE TABLE IF NOT EXISTS`orders`(
+`order_id` INT NOT NULL auto_increment,
+`customer_id` INT NOT NULL,
+`customer_name` VARCHAR(40),
+`order_date` DATE
+); 
 
---to do create tables for orders and order-items table
+CREATE TABLE IF NOT EXISTS `order_lines`(
+`orderline_id` INT NOT NULL auto_increment,
+`order_id` INT,
+`quantity` INT,
+`product_id` INT,
+`price` DECIMAL(10,0),
+PRIMARY KEY(orderline_id),
+FOREIGN KEY(order_id) references orders(order_id),
+FOREIGN KEY(product_id) references products(product_id)
+);
