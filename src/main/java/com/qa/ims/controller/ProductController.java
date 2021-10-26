20f1fa3b45cd5cac3a1928f5db4a7189f77e65b5
samkazshel <samsheldon779@gmail.com>
2021-10-26 20:40:57 +0100

    IQ-21 #comment started work on product controller (added classes from interface)

---
+++ src/main/java/com/qa/ims/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.ProductDAO;
+import com.qa.ims.persistence.domain.Product;
+import com.qa.ims.utils.Utils;
+
+public class ProductController implements CrudController<Product>{
+
+	@Override
+	public List<Product> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	
+
+}
