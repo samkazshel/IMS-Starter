1af7699d99aec90b46f1a11fa654bde9f2cd093a
samkazshel <samsheldon779@gmail.com>
2021-10-29 06:35:04 +0100

    IQ-22 #comment updated IMS file adding order into the constructor

--- src/main/java/com/qa/ims/IMS.java
+++ src/main/java/com/qa/ims/IMS.java
@@ -6,7 +6,9 @@ import org.apache.logging.log4j.Logger;
 import com.qa.ims.controller.Action;
 import com.qa.ims.controller.CrudController;
 import com.qa.ims.controller.CustomerController;
+import com.qa.ims.controller.OrderController;
 import com.qa.ims.persistence.dao.CustomerDAO;
+import com.qa.ims.persistence.dao.OrderDAO;
 import com.qa.ims.persistence.domain.Domain;
 import com.qa.ims.utils.DBUtils;
 import com.qa.ims.utils.Utils;
@@ -16,12 +18,15 @@ public class IMS {
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	private final CustomerController customers;
+	private final OrderController orders;
 	private final Utils utils;
 
 	public IMS() {
 		this.utils = new Utils();
 		final CustomerDAO custDAO = new CustomerDAO();
+		final OrderDAO ordDAO = new OrderDAO();
 		this.customers = new CustomerController(custDAO, utils);
+		this.orders = new OrderController(ordDAO, utils);
 	}
 
 	public void imsSystem() {
@@ -52,6 +57,7 @@ public class IMS {
 			case ITEM:
 				break;
 			case ORDER:
+				active = this.orders;
 				break;
 			case STOP:
 				return;
